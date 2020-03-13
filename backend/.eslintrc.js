053d55a0da569305e38eb23c6f801d1a4adea11a
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-03-13 14:31:44 -0300

    connection to postgress

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   rules: {
     "no-console": "off",
     "prettier/prettier": "error",
-    // "class-methods-use-this": "off",
+    "class-methods-use-this": "off",
     // "no-param-reassign": "off",
     // "camelcase": "off",
     // "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
