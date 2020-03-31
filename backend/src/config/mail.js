2dcf43464c950ec0c694b29e4cc8c9165ada1f9f
Alexandre Akira <alexandreakiraakao@gmail.com>
2020-03-31 00:13:22 -0300

    mail config

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '987acc5b0e7f09',
+    pass: '586a0756bb22d0',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
